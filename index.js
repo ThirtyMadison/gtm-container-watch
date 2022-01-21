c6f3074006fb9feaf37051c2e4b7c92f16c66638
Ryan Wrigley <Ryan.Wrigley@thirtymadison.com>
2022-01-21 11:17:38 -0600

    prettier

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const config = require("./config");
-const { Octokit } = require("@octokit/rest");
-const fetch = require("node-fetch");
-const URL = require("url").URL;
+const config = require("./config")
+const { Octokit } = require("@octokit/rest")
+const fetch = require("node-fetch")
+const URL = require("url").URL
 
 /**
  * Fetch a URL and try to grab the GTM ID assuming the standard implementation snippet
  * @param {string} url
  */
 const getContainerId = async (url) => {
-  const response = await fetch(url);
-  const body = await response.text();
-  const gtmId = body.match(/{ 'optimize_id': '(GTM\-[^']+)'}/i)[1];
-  if (!gtmId) throw "GTM ID not found";
-  return gtmId;
-};
+  const response = await fetch(url)
+  const body = await response.text()
+  const gtmId = body.match(/{ 'optimize_id': '(GTM\-[^']+)'}/i)[1]
+  if (!gtmId) throw "GTM ID not found"
+  return gtmId
+}
 
 /**
  * Fetch a GTM container .js and extract the data object
  * @param {string} url
  */
 const getContainerSnippet = async (gtmId) => {
-  const response = await fetch(config.TAG_MANAGER_BASEURL + gtmId);
-  const body = await response.text();
-  const data = body.match(/var (data = .*),\s{1,2}"runtime":\[/is)[1];
-  if (!data) throw "Data Variable not found";
-  return { data: data, gtmId: gtmId };
-};
+  const response = await fetch(config.TAG_MANAGER_BASEURL + gtmId)
+  const body = await response.text()
+  const data = body.match(/var (data = .*),\s{1,2}"runtime":\[/is)[1]
+  if (!data) throw "Data Variable not found"
+  return { data: data, gtmId: gtmId }
+}
 
 /**
  * Create or Update the fetched data object from GTM .js container after
@@ -34,10 +34,10 @@ const getContainerSnippet = async (gtmId) => {
  * @param {string} host
  */
 const githubCreateOrUpdate = async (data, host) => {
-  const file = Buffer.from(data.data).toString("base64");
+  const file = Buffer.from(data.data).toString("base64")
   const github = new Octokit({
     auth: config.GITHUB_ACCESS_TOKEN,
-  });
+  })
   const githubObj = {
     owner: config.GITHUB_OWNER,
     repo: config.GITHUB_REPO,
@@ -45,8 +45,8 @@ const githubCreateOrUpdate = async (data, host) => {
     message: "Auto Update",
     content: file,
     branch: config.GITHUB_TRACKING_BRANCH,
-  };
-  let result;
+  }
+  let result
 
   github.repos
     .getContents({
@@ -57,17 +57,15 @@ const githubCreateOrUpdate = async (data, host) => {
     })
     .then((item) => {
       if (file !== item.data.content.replace(/\s/g, "")) {
-        result = github.repos.updateFile(
-          Object.assign(githubObj, { sha: item.data.sha })
-        );
+        result = github.repos.updateFile(Object.assign(githubObj, { sha: item.data.sha }))
       }
     })
     .catch((err) => {
-      result = github.repos.createFile(githubObj);
-    });
+      result = github.repos.createFile(githubObj)
+    })
 
-  return result;
-};
+  return result
+}
 
 /**
  * Main function to be scheduled
@@ -75,18 +73,18 @@ const githubCreateOrUpdate = async (data, host) => {
  */
 const gtmWatch = async (event) => {
   for (let i = 0; i < config.TARGETS.length; i++) {
-    let url = new URL(config.TARGETS[i]);
-    let host = url.host;
+    let url = new URL(config.TARGETS[i])
+    let host = url.host
 
-    console.log(`Fetching ${host} now...`);
+    console.log(`Fetching ${host} now...`)
 
     await getContainerId(url.href)
       .then((gtmId) => getContainerSnippet(gtmId))
       .then((data) => githubCreateOrUpdate(data, host))
       .catch((err) => {
-        console.error(err);
-      });
+        console.error(err)
+      })
   }
-};
+}
 
-module.exports.gtmWatch = gtmWatch;
+module.exports.gtmWatch = gtmWatch
