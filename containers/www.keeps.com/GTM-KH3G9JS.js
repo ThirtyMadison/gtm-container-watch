8c982ab9247f8ff89244b01d73cecf4595b6539a
thirtymadison-ci <48422680+thirtymadison-ci@users.noreply.github.com>
2022-01-24 09:42:13 -0500

    Auto Update

---
+++ containers/www.keeps.com/GTM-KH3G9JS.js
@@ -0,0 +1,9 @@
+data = {
+"resource": {
+  "version":"104",
+  
+  "macros":[{"function":"__e"},{"function":"__dee"},{"vtp_experimentKey":"OPT-KH3G9JS_OPT-M5H4T","function":"__c","vtp_value":false},{"function":"__u","vtp_component":"URL","vtp_enableMultiQueryKeys":false,"vtp_enableIgnoreEmptyQueryParam":false},{"function":"__exs","vtp_measurementId":"LYZA5eQUSYKg9-2FuVZx4A","vtp_sections":["list",2]},{"vtp_experimentKey":"OPT-KH3G9JS_OPT-M5H4T","function":"__c","vtp_value":0},{"vtp_experimentKey":"OPT-KH3G9JS_OPT-M5H4T","function":"__c","vtp_value":0},{"function":"__cie","vtp_experimentState":["macro",4],"vtp_trafficCoverage":["macro",5],"vtp_trafficCoverageHash":["macro",6]},{"function":"__c","vtp_value":false},{"function":"__gaoo_c","vtp_trackingId":"UA-105171374-1"},{"function":"__ctto","vtp_isDynamic":false},{"function":"__sel","vtp_selector":":root"},{"function":"__c","vtp_value":["map","",19016]},{"vtp_experimentKey":"OPT-KH3G9JS_OPT-M5H4T","vtp_sectionIndex":0,"function":"__c","vtp_value":0},{"function":"__cevi","vtp_experimentState":["macro",4],"vtp_sectionIndex":0,"vtp_serverVariationIndex":["macro",13]}],
+  "tags":[{"function":"__asprv","vtp_globalName":"google_optimize","vtp_listenForMutations":false,"tag_id":8},{"function":"__asprv","tag_id":9}],
+  "predicates":[{"function":"_eq","arg0":["macro",0],"arg1":["macro",1]},{"function":"_eq","arg0":["macro",0],"arg1":"optimize.callback"}],
+  "rules":[[["if",0],["add",0]],[["if",1],["add",1]]]
+}
